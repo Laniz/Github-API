3edff7cfc9f1e70ffe82470a9b232db895339af4
Shepherd Ncube <68825578+Laniz@users.noreply.github.com>
2025-05-18 17:54:55 -0600

    examining git

--- import requests.py
+++ import requests.py
@@ -8,3 +8,8 @@ print(f"Status code: {r.status_code}")
 response_dict = r.json()
 
 print(response_dict.keys())
+
+print(f"Total repositories: {response_dict["total_count"]}")
+
+repo_dicts = response_dict["items"]
+print(f"Repositories returned: {len(repo_dicts)}")
